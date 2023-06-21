d3032cc2237b22349a5aa48734c3b9d8ba2e7733
tleanne1 <114450881+tleanne1@users.noreply.github.com>
2023-06-20 19:05:00 -0500

    Updated html and JS

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
-window.onload = function () {
-  let form = document.querySelector("form");
-  form.onsubmit = function (event) {
-    // in this section we get the value for each form input
-    const person1Input = document.getElementById("person1Input").value;
-    const person2Input = document.getElementById("person2Input").value;
-    const animalInput = document.getElementById("animalInput").value;
-    const exclamationInput = document.getElementById("exclamationInput").value;
-    const verbInput = document.getElementById("verbInput").value;
-    const adjInput = document.getElementById("adjInput").value;
+function displayRadioValue() {
+  document.getElementById("name").innerHTML =
+    "Customer Name: " + document.getElementById("user_input").value;
+  document.getElementById("checkin").innerHTML =
+    "Check In: " + document.getElementById("user_input2").value;
+  document.getElementById("checkout").innerHTML =
+    "Check Out: " + document.getElementById("user_input3").value;
+  document.getElementById("days").innerHTML =
+    "Total No of Days: " + document.getElementById("user_input4").value;
+  document.getElementById("people").innerHTML =
+    "Total No of People: " + document.getElementById("user_input5").value;
 
-    // then we set the story variables to the values we got from the form
-    document.querySelector("span#person1a").innerText = person1Input;
-    document.querySelector("span#person2a").innerText = person2Input;
-    document.querySelector("span#animal").innerText = animalInput;
-    document.querySelector("span#verb").innerText = verbInput;
-    document.querySelector("span#exclamation").innerText = exclamationInput;
-    document.querySelector("span#adj").innerText = adjInput;
+  document.querySelector("div#story").removeAttribute("class");
 
-    document.querySelector("div#story").removeAttribute("class");
+  var ele = document.getElementsByName("room");
 
-    event.preventDefault();
-  };
-};
+  for (i = 0; i < ele.length; i++) {
+    if (ele[i].checked)
+      document.getElementById("result").innerHTML = "Room: " + ele[i].value;
+  }
+
+  var ele = document.getElementsByName("amenties");
+
+  for (i = 0; i < ele.length; i++) {
+    if (ele[i].checked)
+      document.getElementById("sex.result").innerHTML =
+        "Amenties: " + ele[i].value;
+  }
+}
